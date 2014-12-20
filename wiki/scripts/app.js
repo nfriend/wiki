47fbad0a5de48fbfac4cee993b80979877409c38
Nathan Friend <hello@nathanfriend.io>
2014-12-20 15:24:28 -0600

    added .htaccess for URL rewriting

--- wiki/scripts/app.js
+++ wiki/scripts/app.js
@@ -1,28 +1,62 @@
 ﻿new Vue({
     el: '#app',
     data: {
-        input: '# New page',
-        editor: getURLParameter('editor') === 'true'
+        input: 'Here\'s a new page for you',
+        isEditing: getURLParameter('editor') === 'true',
+        title: getURLParameter('page')
     },
     filters: {
-        marked: marked
+        marked: marked,
+        maxLength: function (text, maxLength) {
+            if (!maxLength)
+                maxLength = 20;
+
+            if (text.length > maxLength) {
+                return text.substring(0, maxLength - 3) + '...'
+            } else {
+                return text;
+            }
+        }
     },
     methods: {
         onBodyKeydown: function(e) {
 
-            // ctrl + shift + e to edit
-            if (e.ctrlKey + e.shiftKey && e.keyCode === 69) {
+            // ctrl + e or ctrl + shift + e to edit
+            if ((e.ctrlKey || (e.ctrlKey && e.shiftKey)) && e.keyCode === 69) {
                 e.preventDefault();
-                this.editor = true;
+                this.edit();
                 return false;
             }
 
             // ctrl + s or ctrl + shift + s to save
             if ((e.ctrlKey || (e.ctrlKey && e.shiftKey)) && e.keyCode === 83) {
                 e.preventDefault();
-                this.editor = false;
+                this.save();
                 return false;
             }
+
+            if ((e.ctrlKey || (e.ctrlKey && e.shiftKey)) && e.keyCode === 83) {
+                e.preventDefault();
+                this.new();
+                return false;
+            }
+        },
+
+        save: function () {
+            this.isEditing = false;
+        },
+        edit: function () {
+            console.log('sdfsdf');
+            this.isEditing = true;
+        },
+        cancel: function() {
+            this.isEditing = false;
+        },
+        delete: function () {
+            
+        },
+        new: function () {
+
         }
     }
 });
